fe27bd4e26322039e717166594c134b71c842c76
HUYNH THAI HOANG <thaih417tohuu417@gmail.com>
2026-09-26 14:12:25 +0700

    Add: longest-repeating-substring-with-replacement - submission-12

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-12.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char, int> cnt;
+        int lengthCharMax = 0; 
+        int l = 0, ans = 0;
+        for (int r = 0; r < s.size(); r++) { 
+            cnt[s[r]]++;
+            lengthCharMax = max(lengthCharMax, cnt[s[r]]);
+            if (r - l + 1 - lengthCharMax > k) { 
+                cnt[s[l]]--;
+                l++;
+            }
+            else ans = max(ans, r - l + 1);
+        }
+        return ans;
+    }
+};
