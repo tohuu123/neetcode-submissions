840d9524d234396d39b6fa7a4ba0652c921beed4
HUYNH THAI HOANG <thaih417tohuu417@gmail.com>
2026-09-26 23:49:04 +0700

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        map<char, int> cnt1;
+
+        for (char x : s1)
+            cnt1[x]++;
+
+        map<char, int> cnt2;
+
+        int l = 0, r = 0;
+
+        while (r < s2.size()) {
+            cnt2[s2[r]]++;
+
+            if (r - l + 1 == s1.size()) {
+
+                int cntSame = 0;
+
+                for (auto &[ch, count] : cnt1) {
+                    if (count == cnt2[ch])
+                        cntSame++;
+                }
+
+                if (cntSame == cnt1.size())
+                    return true;
+
+                // remove LEFTMOST character first
+                cnt2[s2[l]]--;
+                l++;
+            }
+
+            r++;
+        }
+
+        return false;
+    }
+};
